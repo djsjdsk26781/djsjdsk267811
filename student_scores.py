2f222e7a590d6fa7c59b987e0de966c15e2fe570
djsjdsk26781 <164281951+djsjdsk26781@users.noreply.github.com>
2024-03-21 04:28:15 +0100

    Create student_scores.py

---
+++ student_scores.py
@@ -0,0 +1,12 @@
+def add_student_score(student_scores, name, score):
+    """
+    Add a student's score to the dictionary of student scores.
+    
+    Args:
+        student_scores (dict): Dictionary containing student scores.
+        name (str): Name of the student.
+        score (float): Score of the student.
+    """
+    student_scores[name] = score
+
+def find_student_score(student_sc
